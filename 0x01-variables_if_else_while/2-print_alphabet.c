93d8ac5f057b6e25f698e9954fbbb1e464df30a5
Sadiq6570 <sadeeqmuhtar67@gmail.com>
2023-02-20 12:46:11 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,15 +2,17 @@
 
 /**
  * main - print alpha in lower case
- *  Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
 	char lc;
-	for (lc = 'a'; lc <= 'z'; lc++)
-	{
-		putchar(lc)
+for (lc = 'a'; lc <= 'z'; lc++)
+{
+	putchar(lc);
+
 	}
 	putchar('\n');
 	return (0);
 }
+
