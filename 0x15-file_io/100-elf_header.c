af0a5904d6ac16fb7f3d84b197507ca45e9e68bf
sadiq6570 <sadeeqmuhtar67@gmail.com>
2023-05-27 00:51:16 -0700

    123

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,12 +1,3 @@
-	case ET_REL:
-		printf("REL (Relocatable file)\n");
-		break;
-	case ET_EXEC:
-		printf("EXEC (Executable file)\n");
-		break;
-	case ET_DYN:
-		printf("DYN (Shared object file)\n");
-		break;
 	case ET_CORE:
 		printf("CORE (Core file)\n");
 		break;
